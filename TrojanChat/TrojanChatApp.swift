30e8322e7c7a7f22547eb8496e94c2e9c651d0f8
Egg Long <eddres229@gmail.com>
2024-12-19 09:38:42 -0500

    Initial Commit

---
+++ TrojanChat/TrojanChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrojanChatApp.swift
+//  TrojanChat
+//
+//  Created by Eli Dresnin on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct TrojanChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
